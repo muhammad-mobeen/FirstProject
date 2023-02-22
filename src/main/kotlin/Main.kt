5aece731244c5ead62b0605e86ac9d61f2f16536
Muhammad Mobeen <70343951+muhammad-mobeen@users.noreply.github.com>
2023-02-22 10:16:17 +0500

    classTasks

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -69,6 +69,7 @@ fun task2(n: Int){
         {
             println(" ".repeat(noSpaces) + "#".repeat(i))
         }
+
         if(i == n)
         {
             println(" ".repeat((n-3)/2) + "#".repeat(3))
@@ -124,14 +125,70 @@ fun task4(n: Int, m: Int){
     }
 }
 
+fun classTask1(m: Int, n: Int): Int{
+    return m+n
+}
+
+fun classTask2(str: String): String{
+    return str.reversed()
+}
+
+fun classTask3(arr: Array<Int>): Int{
+    var sum = 0
+    for(i in arr)
+    {
+        sum += i
+    }
+    return sum/arr.size
+}
+
+fun classTask4(chr: Char, str: String): Int{
+    return str.filter { it==chr }.count()
+}
+
+fun classTask5(x: Int, y: Int): Int{
+    if(x>y)
+    {
+        return x
+    }
+    else
+    {
+        return y
+    }
+}
+
+fun classTask6(intList: Array<Int>):MutableList<Int>{
+    var evenList = mutableListOf<Int>()
+    for(i in intList)
+    {
+        if(i%2==0)
+        {
+            evenList.add(i)
+        }
+    }
+    return evenList
+}
+
+fun classTask7(str: String):Int{
+    return str.split("\\s+".toRegex()).size
+}
+
 fun main(){
     val n = 10
-    task11(n)
-    task12(n)
-    task13(n)
-    task14(n)
-    task15(n)
-    task2(n)
-    task3(20)
-    task4(1,100)
+//    task11(n)
+//    task12(n)
+//    task13(n)
+//    task14(n)
+//    task15(n)
+//    task2(n)
+//    task3(20)
+//    task4(1,100)
+//    classTask(10,90)
+    println("Task1: Sum of two Integers: "+(classTask1(2,3)).toString())
+    println("Task2: String Reverse: "+ classTask2("Mobeen"))
+    println("Task3: Average of Int Array: "+(classTask3(arrayOf(1,2,3,4))).toString())
+    println("Task4: Count of char: "+(classTask4('e',"Mobeen")).toString())
+    println("Task5: Return Greater Number: "+(classTask5(2,3)).toString())
+    println("Task6: Return Even List: "+(classTask6(arrayOf(1,2,3,4))).toString())
+    println("Task7: Return Number of words: "+(classTask7("I am Muhammad Mobeen")).toString())
 }
